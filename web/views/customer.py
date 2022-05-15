cf8fe6313a425374241887edf7378f35d883bf84
kongsh <kongsuhongbaby@126.com>
2022-05-15 15:41:43 +0800

    修复excel模板下载解析失败的bug

--- web/views/customer.py
+++ web/views/customer.py
@@ -3,7 +3,6 @@ import mimetypes
 from django.shortcuts import render, redirect
 from django.http import FileResponse
 from django.conf import settings
-# import xlrd
 import openpyxl
 
 from web import models
@@ -68,7 +67,6 @@ def customer_import(request):
     :param request:
     :return:
     """
-
     if request.method == 'GET':
         return render(request, 'customer_import.html')
 
@@ -79,11 +77,13 @@ def customer_import(request):
         打开上传的Excel文件，并读取内容
         注：打开本地文件时，可以使用：workbook = xlrd.open_workbook(filename='本地文件路径.xlsx')
         """
+        # print("************文件对象：", dir(customer_excel))
+        # print("************文件对象：", customer_excel.file)
+        # print("************文件对象：", customer_excel.readlines())
+        # print("************文件对象：", customer_excel.chunks)
         # workbook = xlrd.open_workbook(file_contents=customer_excel.file.read())
-        workbook = openpyxl.workbook(file_contents=customer_excel.file.read())
-
-        # sheet = workbook.sheet_by_name('工作表1')
-        sheet = workbook.sheet_by_index(0)
+        workbook = openpyxl.load_workbook(customer_excel)
+        sheet = workbook.worksheets[0]
         row_map = {
             0: {'text': '客户姓名', 'name': 'name'},
             1: {'text': '年龄', 'name': 'age'},
@@ -91,15 +91,18 @@ def customer_import(request):
             3: {'text': '公司', 'name': 'company'},
         }
         object_list = []
-        for row_num in range(1, sheet.nrows):
-            row = sheet.row(row_num)
+        for row in list(sheet.rows)[1:]:
             row_dict = {}
             for col_num, name_text in row_map.items():
+                if not row[col_num].value:
+                    break
                 row_dict[name_text['name']] = row[col_num].value
-            object_list.append(models.Customer(**row_dict))
+            else:
+                object_list.append(models.Customer(**row_dict))
 
         models.Customer.objects.bulk_create(object_list, batch_size=20)
     except Exception as e:
+        print('导入文件异常：', e)
         context['status'] = False
         context['msg'] = '导入失败'
 
@@ -114,7 +117,7 @@ def customer_tpl(request):
     """
     tpl_path = os.path.join(settings.BASE_DIR, 'web', 'files', '批量导入客户模板.xlsx')
     content_type = mimetypes.guess_type(tpl_path)[0]
-    print(content_type)
+    print('********[customer_tpl]-content_type:', content_type)
     response = FileResponse(open(tpl_path, mode='rb'), content_type=content_type)
-    response['Content-Disposition'] = "attachment;filename=%s" % 'customer_excel_tpl.xlsx'
+    response['Content-Disposition'] = "attachment;filename=%s" % 'customer_excel_example.xlsx'
     return response
